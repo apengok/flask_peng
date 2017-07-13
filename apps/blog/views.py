10fd845ce5dba29b97ec15121433fb400815f315
apengok <apengok@163.com>
2017-07-13 21:35:00 +0800

    remove extra files

--- apps/blog/views.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template,redirect,url_for
-
-from . import blog
-
-@blog.route('/')
-@blog.route('/blog/',methods=['GET','POST'])
-def index():
-    return render_template('blog/index.html')
-
