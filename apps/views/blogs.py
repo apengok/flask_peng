dfc645fd01163a9df7792da21687c19b396d29e0
apengok <apengok@163.com>
2017-07-25 22:45:51 +0800

    blogs new

--- apps/views/blogs.py
+++ apps/views/blogs.py
@@ -1,8 +1,14 @@
 from flask import Blueprint,render_template,redirect,url_for
+from flask_login import login_required
 
 mod = Blueprint('blog',__name__)
 
 @mod.route('/blog/',methods=['GET','POST'])
 def index():
     return render_template('blog/index.html')
+    
+@mod.route('/blog/new_post',methods=['GET','POST'])
+@login_required
+def new_blog():
+    return render_template('blog/new_blog.html')
 
