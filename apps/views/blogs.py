05da10b84f9896591f0460f3d2a8a4640ac49a64
apengok <apengok@163.com>
2017-07-31 00:49:28 +0800

    blog paginate

--- apps/views/blogs.py
+++ apps/views/blogs.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,redirect,url_for
+from flask import Blueprint,render_template,redirect,url_for,request
 from flask_login import login_required,current_user
 from apps import db
 from apps.forms.blogs import PostBlogForm
@@ -8,8 +8,13 @@ mod = Blueprint('blog',__name__)
 
 @mod.route('/blog/',methods=['GET','POST'])
 def index():
-    blogs = current_user.posts.order_by(BlogPost.timestamp.desc()).all()
-    return render_template('blog/index.html',blogs=blogs)
+    page = request.args.get('page',1,type=int)
+    pagination = current_user.posts.order_by(BlogPost.timestamp.desc()).paginate(
+            page,per_page=1,error_out=False
+        )
+    blogs =pagination.items
+    #blogs = current_user.posts.order_by(BlogPost.timestamp.desc()).all()
+    return render_template('blog/index.html',blogs=blogs,pagination=pagination)
     
 @mod.route('/blog/new_blog/',methods=['GET','POST'])
 @login_required
@@ -23,9 +28,11 @@ def new_blog():
         description = form.description.data
         
         post = BlogPost(headline=title,body=body,author=current_user._get_current_object())
+        post.keywords.append(Keyword(tags))
         db.session.add(post)
         db.session.commit()
         
         return redirect(url_for('blog.index'))
+    print 'invalid submit?'
     return render_template('blog/new_blog.html',form=form)
 
